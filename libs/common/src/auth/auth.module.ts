bedfad5a8b787599a438071bab96777043b1214a
yuvraj9011 <yuvrajjavalkote0@gmail.com>
2024-01-10 19:00:28 +0530

    micoservicesusingdocker

---
+++ libs/common/src/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { RmqModule } from '../rmq/rmq.module';
+import { AUTH_SERVICE } from './services';
+
+@Module({
+  imports: [RmqModule.register({ name: AUTH_SERVICE })],
+  exports: [RmqModule],
+})
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    console.log("Yuvraj common2")
+    consumer.apply(cookieParser()).forRoutes('*');
+  }
+}
